39bf42bf3c46abbbca110a2f2adc592aa3b1f5ee
waseem <waseemazmy21@gmail.com>
2023-02-23 01:48:51 +0200

    Create main content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './sass/style.scss';
 import './images/favicon-32x32.png';
+import './images/icon-up.svg';
+import './images/icon-down.svg';
+import './images/icon-facebook.svg';
+import './images/icon-twitter.svg';
+import './images/icon-instagram.svg';
+import './images/icon-youtube.svg';
 
